dcaf0a2e729be8b110e458f6d363a326d6f19819
Samo Fabcic <samo.fabcic@gmail.com>
2015-07-14 09:40:37 +0200

    added gitignore

--- nuget_publish.bat
+++
@@ -1,4 +0,0 @@
-md .\bin\nuget
-del .\bin\nuget\*.nupkg
-.nuget\NuGet.exe pack -Build -OutputDirectory .\bin\nuget\  -Prop Configuration=Release -IncludeReferencedProjects -Verbosity detailed
-.nuget\NuGet.exe push .\bin\nuget\*.nupkg DC838B29422CB7C5E357686CEE165 -Source http://nuget.t.renderspace.net
