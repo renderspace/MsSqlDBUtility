8de1f538db4b2303af160dd46582aadef1276d9a
Samo Fabcic <samo.fabcic@gmail.com>
2015-07-17 14:15:11 +0200

    nuget and gitingore

--- nuget_publish.bat
+++ nuget_publish.bat
@@ -1,4 +1,4 @@
-md .\bin\nuget
-del .\bin\nuget\*.nupkg
-.nuget\NuGet.exe pack -Build -OutputDirectory .\bin\nuget\  -Prop Configuration=Release -IncludeReferencedProjects -Verbosity detailed
-.nuget\NuGet.exe push .\bin\nuget\*.nupkg DC838B29422CB7C5E357686CEE165 -Source http://nuget.t.renderspace.net
+rem md .\bin\nuget
+rem del .\bin\nuget\*.nupkg
+rem .nuget\NuGet.exe pack -Build -OutputDirectory .\bin\nuget\  -Prop Configuration=Release -IncludeReferencedProjects -Verbosity detailed
+.nuget\NuGet.exe push .\*.nupkg DC838B29422CB7C5E357686CEE165 -Source http://nuget.t.renderspace.net
